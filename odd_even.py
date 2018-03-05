9321162603e2ef4c888ed2b2c33eba497e82812b
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-03-05 02:39:29 -0800

    odd_even.py
    odd_even

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+s=raw_input()
+n=len(s)
+print "even:"
+for i in range(0,n):
+      if(i%2==0):
+          print s[i]
+print "odd:"
+for k in range(0,n):
+      if(k%2!=0):
+         print s[k]
+      
